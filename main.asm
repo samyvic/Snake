f945cacb9713e6761802dfd88ac959ed016a69dc
root <root@Ahmed.localdomain>
2021-01-04 16:33:32 +0200

    The main project structure

---
+++ main.asm
@@ -0,0 +1,49 @@
+TITLE Snake.asm
+INCLUDE Irvine32.inc
+.DATA
+.CODE
+
+;-----Start [Main proc]-----
+main PROC
+
+main ENDP
+;-----End [Main proc]-----
+
+;-----Start [initSnake proc]-----
+
+;-----End [initSnake proc]-----
+
+
+;-----Start [clearMemory proc]-----
+
+;-----End [clearMemory proc]-----
+
+;-----Start [startGame proc]-----
+
+;-----End [startGame proc]-----
+
+;-----Start [moveSnake proc]-----
+
+;-----End [moveSnake proc]-----
+
+;-----Start [createFood proc]-----
+
+;-----End [createFood proc]-----
+
+;-----Start [accessIndex proc]-----
+
+;-----End [accessIndex proc]-----
+
+;-----Start [saveIndex proc]-----
+
+;-----End [saveIndex proc]-----
+
+;-----Start [paint proc]-----
+
+;-----End [paint proc]-----
+
+;-----Start [generateLevel proc]-----
+
+;-----End [generateLevel proc]-----
+
+END main
