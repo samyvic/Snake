a0e7b39e36ceff0c3cd2f38450ff1b1b4d980eb4
root <root@Ahmed.localdomain>
2021-01-05 20:12:26 +0200

    adding init snake proc

--- main.asm
+++ main.asm
@@ -10,7 +10,34 @@ main ENDP
 ;-----End [Main proc]-----
 
 ;-----Start [initSnake proc]-----
+initSnake PROC USES EBX EDX
 
+; This procedure initializes the snake to the default position
+; in the center of the screen
+
+    MOV DH, 13      ; Set row number to 13
+    MOV DL, 47      ; Set column number to 47
+    MOV BX, 1       ; First segment of snake
+    CALL saveIndex  ; Write to framebuffer
+
+    MOV DH, 14      ; Set row number to 14
+    MOV DL, 47      ; Set column number to 47
+    MOV BX, 2       ; Second segment of snake
+    CALL saveIndex  ; Write to framebuffer
+
+    MOV DH, 15      ; Set row number to 15
+    MOV DL, 47      ; Set column number to 47
+    MOV BX, 3       ; Third segment of snake
+    CALL saveIndex  ; Write to framebuffer
+
+    MOV DH, 16      ; Set row number to 16
+    MOV DL, 47      ; Set column number to 47
+    MOV BX, 4       ; Fourth segment of snake
+    CALL saveIndex  ; Write to framebuffer
+
+    RET
+
+initSnake ENDP
 ;-----End [initSnake proc]-----
 
 
